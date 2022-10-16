6c92e5062b79dbc3c53bfaf106bcc5e899d4ac39
Akash Prajapati <66644705+i1s-akash@users.noreply.github.com>
2022-10-16 16:20:24 +0530

    Update Subscribe.jsx

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -5,8 +5,8 @@ function Subscribe() {
     <div id="subscribe">
         <h3>Subscribe</h3>
         <div className="subscribe-input">
-            <input type="email" placeholder="example@gmail.com"/>
-            <a href="#">Continue</a>
+            <input type="email" placeholder="xyz@gmail.com"/>
+            <a href="#">Get Code</a>
         </div>
     </div>
   )
